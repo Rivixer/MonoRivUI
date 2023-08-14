683f3f59bd0ae71df2d78f6b30ba5ae78418f7d6
Wiktor Jaworski <wijaweq@gmail.com>
2023-08-14 13:43:00 +0200

    Add KeyboardController

--- AnyPoly/AnyPoly.cs
+++ AnyPoly/AnyPoly.cs
@@ -1,40 +1,68 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-#nullable disable
-
 namespace AnyPoly;
 
+/// <summary>
+/// Represents the main game class.
+/// </summary>
 public class AnyPoly : Game
 {
-    public static AnyPoly Instance { get; private set; }
+    private readonly GraphicsDeviceManager graphics;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AnyPoly"/> class.
+    /// </summary>
     public AnyPoly()
     {
         Instance = this;
 
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        this.graphics = new GraphicsDeviceManager(this);
+
+        this.Content.RootDirectory = "Content";
+        this.IsMouseVisible = true;
     }
 
+    /// <summary>
+    /// Gets the singleton instance of the <see cref="AnyPoly"/> class.
+    /// </summary>
+    public static AnyPoly Instance { get; private set; } = default!;
+
+    /// <summary>
+    /// Initializes the game.
+    /// </summary>
     protected override void Initialize()
     {
         base.Initialize();
     }
 
+    /// <summary>
+    /// Loads the game content.
+    /// </summary>
     protected override void LoadContent()
     {
-
+        base.LoadContent();
     }
 
+    /// <summary>
+    /// Updates the game state.
+    /// </summary>
+    /// <param name="gameTime">The game time.</param>
+    /// <remarks>It is called once per frame.</remarks>
     protected override void Update(GameTime gameTime)
     {
+        KeyboardController.Update();
         base.Update(gameTime);
     }
 
+    /// <summary>
+    /// Draws the game.
+    /// </summary>
+    /// <param name="gameTime">The game time.</param>
+    /// <remarks>It is called once per frame.</remarks>
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        this.GraphicsDevice.Clear(Color.CornflowerBlue);
 
         base.Draw(gameTime);
     }
---
+++ AnyPoly/Source/UI/Controllers/KeyboardController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace AnyPoly;
+
+/// <summary>
+/// A static class responsible for managing keyboard input.
+/// </summary>
+internal class KeyboardController
+{
+    private static KeyboardState previousKeyboard;
+    private static KeyboardState currentKeyboard;
+
+    /// <summary>
+    /// Updates the keyboard state.
+    /// </summary>
+    /// <remarks>
+    /// This method should be called once per frame
+    /// to keep the keyboard state up to date.
+    /// </remarks>
+    public static void Update()
+    {
+        previousKeyboard = currentKeyboard;
+        currentKeyboard = Keyboard.GetState();
+    }
+
+    /// <summary>
+    /// Checks if the specified key is currently down.
+    /// </summary>
+    /// <param name="key">The key to check.</param>
+    /// <returns>
+    /// <see langword="true"/> if the key is down;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsKeyDown(Keys key)
+    {
+        return currentKeyboard.IsKeyDown(key);
+    }
+
+    /// <summary>
+    /// Checks if the specified key is currently up.
+    /// </summary>
+    /// <param name="key">The key to check.</param>
+    /// <returns>
+    /// <see langword="true"/> if the key is up;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsKeyUp(Keys key)
+    {
+        return currentKeyboard.IsKeyUp(key);
+    }
+
+    /// <summary>
+    /// Checks if the specified key has been hit.
+    /// </summary>
+    /// <param name="key">The key to check.</param>
+    /// <returns>
+    /// <see langword="true"/> if the key has been hit;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    /// <remarks>
+    /// The key has been hit if it was up in the previous frame
+    /// and is down in the current frame.
+    /// </remarks>
+    public static bool IsKeyHit(Keys key)
+    {
+        return previousKeyboard.IsKeyUp(key)
+            && currentKeyboard.IsKeyDown(key);
+    }
+}
