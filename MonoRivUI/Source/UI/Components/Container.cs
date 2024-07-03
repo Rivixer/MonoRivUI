5d398cd82e3f5392e0e1bd425d2d52db5d12f1df
Wiktor Jaworski <wijaweq@gmail.com>
2024-07-04 00:06:16 +0200

    Add IButtonContent to Container

--- MonoRivUI/Source/UI/Components/Container.cs
+++ MonoRivUI/Source/UI/Components/Container.cs
@@ -1,8 +1,16 @@
-﻿namespace MonoRivUI;
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoRivUI;
 
 /// <summary>
 /// Represents a UI container component.
 /// </summary>
-public class Container : Component
+public class Container : Component, IButtonContent<Container>
 {
+    /// <inheritdoc/>
+    /// <remarks>The container component is always hovered.</remarks>
+    public bool IsButtonContentHovered(Point mousePosition)
+    {
+        return true;
+    }
 }
