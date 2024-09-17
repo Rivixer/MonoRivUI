266ded7b366aecd25a5cc3960179ebe60256f25e
Wiktor Jaworski <wijaweq@gmail.com>
2024-09-17 11:45:03 +0200

    Make Container styleable

--- MonoRivUI/Source/UI/Components/Container.cs
+++ MonoRivUI/Source/UI/Components/Container.cs
@@ -5,12 +5,18 @@ namespace MonoRivUI;
 /// <summary>
 /// Represents a UI container component.
 /// </summary>
-public class Container : Component, IButtonContent<Container>
+public class Container : Component, IButtonContent<Container>, IStyleable<Container>
 {
     /// <inheritdoc/>
     /// <remarks>The container component is always hovered.</remarks>
     public bool IsButtonContentHovered(Point mousePosition)
     {
         return true;
+    }
+
+    /// <inheritdoc/>
+    public void ApplyStyle(Style<Container> style)
+    {
+        style.Apply(this);
     }
 }
